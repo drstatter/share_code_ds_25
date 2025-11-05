9f1ef8f7de8992a21b1b91a6f34acdf2ef7b3e54
statt <statter@gmail.com>
2025-11-05 10:19:01 +0200

    add binSearchIter

---
+++ src/rec/reverseNumber.java
@@ -0,0 +1,16 @@
+package rec;
+
+public class reverseNumber {
+    //this function will reverse a number using recation
+    public int reverse(int number) {
+        return reverseRec(number, 0);
+    }
+    private int reverseRec(int number, int rev) {
+        if (number == 0) {
+            return rev;
+        } else {
+            rev = rev * 10 + number % 10;
+            return reverseRec(number / 10, rev);
+        }
+    }
+}
