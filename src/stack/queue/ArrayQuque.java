37c3cea9144b51835ed5dada33eed584b067006f
statt <statter@gmail.com>
2025-12-15 10:37:43 +0200

    add queue

---
+++ src/stack/queue/ArrayQuque.java
@@ -0,0 +1,56 @@
+package stack.queue;
+
+public class ArrayQuque {
+    private int[] data;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+    public ArrayQuque(int capacity) {
+        this.capacity = capacity;
+        data = new int[capacity];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+    public void enqueue(int value) {
+        if (size == capacity) {
+            throw new RuntimeException("Queue is full");
+        }
+        rear = (rear +1) % capacity;
+        data[rear] = value;
+        size++;
+    }
+    public int dequeue() {
+        if (size == 0) {
+            throw new RuntimeException("Queue is empty");
+        }
+        int value = data[front];
+        front = (front +1) % capacity;
+        size--;
+        return value;
+    }
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int peek() {
+        if (size == 0) {
+            throw new RuntimeException("Queue is empty");
+        }
+        return data[front];
+    }
+
+
+    public static void main(String[] args) {
+        ArrayQuque queue = new ArrayQuque(5);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.peek());
+        System.out.println(queue.size());
+    }
+}
